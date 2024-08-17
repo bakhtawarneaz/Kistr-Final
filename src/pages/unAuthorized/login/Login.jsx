8e0767a14f6c7a192fa0e1d60cc70b3280d3f221
bakhtawarneaz <bakhtawarneaz@gmail.com>
2024-08-17 13:07:59 +0500

    test

--- src/pages/unAuthorized/login/Login.jsx
+++ src/pages/unAuthorized/login/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {logo} from '../../../assets/kistr-log.svg'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login-header">
+      <h1><img src={logo} alt="kistr-logo" /></h1>
+    </div>
   )
 }
 
